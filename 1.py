2759021bd1f38221198583920d754879a9ad0372
Kamino154GabrielW <gabriel.wroz@gmail.com>
2025-11-02 14:20:31 +0100

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,13 +1,39 @@
 import pygame
 import time
 import random
+from pygame import mixer
+pygame.mixer.init()
+pygame.font.init()
 
-print("zklonowana wersja ale i tak jej niepoprawiłem i przepraszam że niejest dokładnie jak w poleceniu ale niema tu sensownch printów do edcji")
+Strzal_dzwienk= pygame.mixer.Sound('bullet-hit-metal-84818.mp3')
+
+mixer.music.load('cowboy-rythym-solo-louder-ver-25312.mp3') 
+pygame.mixer.music.set_volume(2)  
+mixer.music.play(-1)
+
 x_okna=1300
 y_okna=800
 
-x_celownik=0
-y_celownik=0
+x_celownik=-100
+y_celownik=-100
+
+x_wnik=0
+y_wnik=0
+wynik=0
+
+X_START, X_END = 400, 800
+Y_START, Y_END = 210, 590
+
+
+
+
+X_MID = 600
+Y_MID = 400
+
+
+OFFSET_SUBTRACT = 150
+font = pygame.font.SysFont("Comic Sans MS", 24)
+
 
 def high_noon():
     cel_dolny = pygame.image.load("celownik_dolny.png")
@@ -19,34 +45,42 @@ def high_noon():
         while True:
             bilt_tlo()
             if orientacja == "pion":
-                okno.blit(obraz, (0, pozycja))
+                okno.blit(obraz, (0, pozycja-10))
             else:
-                okno.blit(cel_dolny, (0, y))  
-                okno.blit(obraz, (pozycja, 0))
+                okno.blit(cel_dolny, (0, y-10))  
+                okno.blit(obraz, (pozycja-10, 0))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    return pozycja
+                    return pozycja+26
 
             pygame.time.wait(prendkosci)
-            pozycja -= 5
+            pozycja -= 10
             pygame.display.update()
 
-    y = przesun_celownik(y, -5, cel_dolny, "pion",2)
-    x = przesun_celownik(x, -5, cel_boczny, "poziom",2)
+    y = przesun_celownik(y, -10, cel_dolny, "pion",8)
+    x = przesun_celownik(x, -10, cel_boczny, "poziom",8)
 
-    pygame.time.wait(100)
+    pygame.time.wait(500)
     okno.fill((255,255,255))
+    pygame.mixer.Sound.play(Strzal_dzwienk)
     pygame.display.update()    
-    pygame.time.wait(1000)
+    pygame.time.wait(600)
+    if x<=0:
+        x=0
+    if y<=0:
+        y=0
     return x, y
 
 def bilt_tlo():
         okno.blit(tlo,(0,0))
-        okno.blit(cel_obraz,(200,200))
+        okno.blit(cel_obraz,(203,200))
+        if  x_celownik !=0 and y_celownik!=0:
+            okno.blit(dziura, (x_celownik-10, y_celownik-10))
+        okno.blit(wnik_text,(500,100))
         
 
 
@@ -54,8 +88,13 @@ tlo=pygame.image.load("background_1.png")
 cel_obraz=pygame.image.load("tarcza_sztrzelecka.png")
 okno = pygame.display.set_mode((x_okna,y_okna))
 loop=True
+dziura=pygame.image.load("trafienie.png")
+
 while loop==True:
+        wnik_str="wynik:"+str(wynik)
+        wnik_text = font.render(wnik_str, True, (255, 255, 255))
         bilt_tlo()
+        
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -63,17 +102,51 @@ while loop==True:
                 exit()
             if event.type ==pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                            x_celownik,y_celownik=high_noon()
-                            print(x_celownik)
-                            print(y_celownik)
+                        x_celownik, y_celownik = high_noon()
+
+                        print(f"Celownik: ({x_celownik}, {y_celownik})")
+
+   
+                        if X_START < x_celownik < X_END and Y_START < y_celownik < Y_END:
+        
+        
+                            x_dist_center = abs(x_celownik - X_MID) 
+                            y_dist_center = abs(y_celownik - Y_MID) 
+
+       
+                            x_wnik = 200 - x_dist_center 
+        
+        
+                            y_wnik = 190 - y_dist_center 
+
+        
+                            wynik = y_wnik + x_wnik
+                            wynik -= OFFSET_SUBTRACT
+
+        
+                            wynik = max(0, wynik)
+        
+                        else:
+                            wynik = 0
+
+                        print(f"Wynik: {wynik}")
+
+
+
+                
+
+
+
+
+        
+        
 
 
                             
                     
 
-
-        
         pygame.time.wait(40)
+        
         
 
         
